6f639b81f98a7663344863cae241121e28262208
mujeebmasi <shaikhabdulmujeeb0415@gmail.com>
2026-05-13 11:06:58 +0530

    mongodb atlas connected

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,16 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 MONGO_URL = os.getenv("MONGODB_URI")
+
 if not MONGO_URL:
-	raise RuntimeError("MONGODB_URI environment variable is required")
+    raise RuntimeError("MONGODB_URI environment variable is required")
 
 client = MongoClient(MONGO_URL)
+
 db = client.second_brain
-db.users.create_index("username", unique = True)
+
+db.users.create_index("username", unique=True)
